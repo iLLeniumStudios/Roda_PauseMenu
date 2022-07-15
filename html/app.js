cac1b9bac01d4e96e52a667cfe6af9f7ce0e693d
TheiLLeniumStudios <104288623+TheiLLeniumStudios@users.noreply.github.com>
2022-07-15 00:17:16 +0000

    chore(ui): Format JS

--- html/app.js
+++ html/app.js
@@ -1,56 +1,56 @@
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
   console.log('Roda PauseMenu Started')
 })
 
-window.addEventListener('message', function(event) {
+window.addEventListener('message', function (event) {
   var v = event.data
 
-  switch(v.action) {
+  switch (v.action) {
 
-      case 'show':
-          ShowSettings()
-          setTimeout(function() {
-            $(document).bind("keyup", escapeHandler);
-          }, 1000);
+    case 'show':
+      ShowSettings()
+      setTimeout(function () {
+        $(document).bind("keyup", escapeHandler);
+      }, 1000);
       break;
 
-      case 'UpdateData':
-          setValue(v.key, v.value)
+    case 'UpdateData':
+      setValue(v.key, v.value)
       break;
 
-      case 'updateJobs':
-          $('.appendJobs').append(`
+    case 'updateJobs':
+      $('.appendJobs').append(`
             <h1 class="RemoveShit">${v.label} - ${v.value}</h1>
           `)
       break;
 
-      case 'updatePlayers': 
-          $('.footer h2').text(`Connected Players ${v.total}/${v.max}`)
+    case 'updatePlayers':
+      $('.footer h2').text(`Connected Players ${v.total}/${v.max}`)
       break;
   }
 })
 
 function escapeHandler(e) {
   if (e.key === "Escape") {
-		CloseAll();
-	}
+    CloseAll();
+  }
 }
 
-function setValue(key, value){
-	if ($('#'+key+'').text() != value) {
-		if (!isNaN(value)){
-			var $this = $('#'+key+'');
-			jQuery({ Counter: parseInt($this.text(), 10) }).animate({ Counter: value }, {
-				duration: 2000,
-				easing: 'swing',
-				step: function (now) {
-					$this.text('$ '+Math.ceil(now));
-				}
-			});
-		} else {
-			$('#'+key+'').text(''+value+'')
-		}
-	}
+function setValue(key, value) {
+  if ($('#' + key + '').text() != value) {
+    if (!isNaN(value)) {
+      var $this = $('#' + key + '');
+      jQuery({ Counter: parseInt($this.text(), 10) }).animate({ Counter: value }, {
+        duration: 2000,
+        easing: 'swing',
+        step: function (now) {
+          $this.text('$ ' + Math.ceil(now));
+        }
+      });
+    } else {
+      $('#' + key + '').text('' + value + '')
+    }
+  }
 }
 
 function ShowSettings() {
@@ -58,29 +58,29 @@ function ShowSettings() {
   openUi = true
 }
 
-$(function(){
-    $('#settingsPe').click(function(){
-      $.post('https://Roda_PauseMenu/SendAction', JSON.stringify({action: 'settings'}));
-      CloseAll()
-    })
-    $('#mapita').click(function(){
-      $.post('https://Roda_PauseMenu/SendAction', JSON.stringify({action: 'map'}));
-      CloseAll()
-    })
-    $('#resume').click(function(){
-      CloseAll()
-    })
+$(function () {
+  $('#settingsPe').click(function () {
+    $.post('https://Roda_PauseMenu/SendAction', JSON.stringify({ action: 'settings' }));
+    CloseAll()
+  })
+  $('#mapita').click(function () {
+    $.post('https://Roda_PauseMenu/SendAction', JSON.stringify({ action: 'map' }));
+    CloseAll()
+  })
+  $('#resume').click(function () {
+    CloseAll()
+  })
 
-    $('#discordinv').click(function(){
-        window.invokeNative('openUrl', 'https://discord.gg/veDufrHnAn')
-    })
+  $('#discordinv').click(function () {
+    window.invokeNative('openUrl', 'https://discord.gg/veDufrHnAn')
+  })
 
-    $('#tebexshop').click(function(){
-      window.invokeNative('openUrl', 'https://roda.tebex.io/')
-    })
+  $('#tebexshop').click(function () {
+    window.invokeNative('openUrl', 'https://roda.tebex.io/')
+  })
 })
 
-function CloseAll(){
+function CloseAll() {
   $(document).unbind("keyup", escapeHandler);
   $('.RemoveShit').remove()
   $('.container-fluid').hide(500)
